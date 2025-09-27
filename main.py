47a70a91f055e368da3f2b46c6e1425857823526
Kaze Farel <ryuu.the.demonslayer@gmail.com>
2025-09-27 00:28:25 -0600

    Finished the asteroid.py and created the asteroidfield.py, implementing them into main.py; added def check_collisions to circleshape.py for all objects using it as a parent to inherit, then utilized it to print "Game Over!" and close the game upon player-asteroid impacts.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     print(f"""Starting Asteroids!
@@ -12,24 +15,33 @@ Screen height: {SCREEN_HEIGHT}""")
     clock = pygame.time.Clock()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)    
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-    print(len(updatable), len(drawable))
-
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
+        
+        for asteroid in asteroids:
+            if player.check_collision(asteroid):
+                print("player", player.position, player.radius)
+                for a in asteroids:
+                    print("asteroid", a.position, a.radius, player.check_collision(a))
+                print("Game Over!")
+                sys.exit()
 
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
-        updatable.update(dt)
-
 
 if __name__ == "__main__":
     main()
